627de7ec19b49a381c2867f64d1c121108e7646c
João Fernandes <joaofergear003@gmail.com>
2025-02-06 12:25:53 -0300

    test `exercise_performance_metrics`

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { strict } from "node:assert";
+import { describe, it } from "node:test";
+
 function date_is_in_range(date, range)
 {
 	const date_ms = date.getTime();
@@ -70,3 +73,125 @@ function exercise_performance_metrics(sessions, query)
 
 	return result;
 }
+
+describe("Exercise performance metrics", () => {
+	const sessions_mock = [{
+		performed_at: new Date(2025, 0, 1),
+		exercises: [{
+			name: "Push-up",
+			max_reps: 10,
+			max_weight: 0
+		}, {
+			name: "Pull-up",
+			max_reps: 6,
+			max_weight: 0
+		}]
+	}, {
+		performed_at: new Date(2025, 0, 3),
+		exercises: [{
+			name: "Push-up",
+			max_reps: 12,
+			max_weight: 0
+		}, {
+			name: "Pull-up",
+			max_reps: 6,
+			max_weight: 0
+		}]
+	}, {
+		performed_at: new Date(2025, 0, 5),
+		exercises: [{
+			name: "Push-up",
+			max_reps: 13,
+			max_weight: 0
+		}, {
+			name: "Pull-up",
+			max_reps: 6,
+			max_weight: 2.5
+		}]
+	}, {
+		performed_at: new Date(2025, 0, 8),
+		exercises: [{
+			name: "Push-up",
+			max_reps: 12,
+			max_weight: 0
+		}, {
+			name: "Pull-up",
+			max_reps: 8,
+			max_weight: 0
+		}]
+	}];
+
+	const { metrics } = exercise_performance_metrics(sessions_mock, {
+		session_start: sessions_mock[0].performed_at,
+		session_end: sessions_mock[sessions_mock.length-1].performed_at
+	});
+
+	const push_up = metrics.get("Push-up");
+	const pull_up = metrics.get("Pull-up");
+
+	it("contain the new max of reps that can be performed", () => {
+		strict.equal(push_up.new_max_reps, 13);
+		strict.equal(pull_up.new_max_reps, 8);
+	});
+
+	it("contain the new max of weight that can be lifted", () => {
+		strict.equal(push_up.new_max_weight, 0);
+		strict.equal(pull_up.new_max_weight, 2.5);
+	});
+
+	it("count how many reps was progressed", () => {
+		strict.equal(push_up.reps_progressed, 3);
+		strict.equal(pull_up.reps_progressed, 2);
+	});
+
+	it("count how many weight was progressed", () => {
+		strict.equal(push_up.weight_progressed, 0);
+		strict.equal(pull_up.weight_progressed, 2.5);
+	});
+
+	it("count how many progressions was achieved in reps", () => {
+		strict.equal(push_up.progressions_in_reps, 2);
+		strict.equal(pull_up.progressions_in_reps, 1);
+	});
+
+	it("count how many progressions was achieved in weight", () => {
+		strict.equal(push_up.progressions_in_weight, 0);
+		strict.equal(pull_up.progressions_in_weight, 1);
+	});
+
+	it("count how many plateau was achieved in reps", () => {
+		strict.equal(push_up.plateau_in_reps, 1);
+		strict.equal(pull_up.plateau_in_reps, 2);
+	});
+
+	it("count how many plateau was achieved in weight", () => {
+		strict.equal(push_up.plateau_in_weight, 3);
+		strict.equal(pull_up.plateau_in_weight, 2);
+	});
+
+	it("returns the metrics for a specific date range", () => {
+		const { from, to, metrics } = exercise_performance_metrics(sessions_mock, {
+			session_start: sessions_mock[1].performed_at,
+			session_end: sessions_mock[2].performed_at
+		});
+
+		const push_up = metrics.get("Push-up");
+		const pull_up = metrics.get("Pull-up");
+
+		strict.equal(from.getTime(), sessions_mock[1].performed_at.getTime());
+		strict.equal(to.getTime(), sessions_mock[2].performed_at.getTime());
+		strict.equal(push_up.new_max_reps, 13);
+		strict.equal(pull_up.new_max_reps, 6);
+	});
+
+	it("contain filtered metrics for a specific exercise", () => {
+		const { metrics } = exercise_performance_metrics(sessions_mock, {
+			exercise_names: ["Push-up"],
+			session_start: sessions_mock[0].performed_at,
+			session_end: sessions_mock[sessions_mock.length-1].performed_at
+		});
+
+		strict.equal(metrics.size, 1);
+		strict.equal(metrics.has("Push-up"), true);
+	});
+});
